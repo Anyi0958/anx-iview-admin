70258789256ab7216c05e04a95fe367e6a8fdca6
AnOne <535354689@qq.com>
2021-01-16 14:31:35 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ touch .nojekyll
 git init
 git add -A
 git commit -m 'deploy'
-git push -f "https://gitee.com/anitt/anx-iview-admin.git" master:ay-pages
+git push -f "https://gitee.com/anitt/anx-iview-admin.git" master:anx-pages
 start "https://gitee.com/anitt/anx-iview-admin/pages"
-git push -f "https://github.com/Anyi0958/anx-iview-admin.git" master:ay-pages
+git push -f "https://github.com/Anyi0958/anx-iview-admin.git" master:anx-pages
 cd -
 exec /bin/bash
 
